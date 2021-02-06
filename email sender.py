a65f1ce6584d07a33051f833b2cef17f97861539
Chav Sumalinog <chaviotics@gmail.com>
2021-02-06 11:55:52 +0800

    Added SMTP.

--- email sender.py
+++ email sender.py
@@ -6,3 +6,15 @@ email['from'] = 'Chav Sumalinog'
 email['to'] = 'chaviotics@gmail.com'
 email['subject'] = 'First email from python.'
 
+email.set_content('Hello Chav!') # can send stuff
+
+# smtp sends to email
+
+with smtplib.SMTP(host='smpt.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls() # encryption mechanism
+    smtp.login('icearchts@gmail.com', 'emiliepp2')
+    smtp.send_message(email)
+    print('All good!')
+
+
