667640df6c12208ae98d8c268eadf67f2bd35d10
Chav Sumalinog <chaviotics@gmail.com>
2021-02-06 12:12:23 +0800

    Added html functionality for more customization.

--- email sender.py
+++ email sender.py
@@ -1,12 +1,16 @@
 import smtplib # allows us to create an SMT server
 from email.message import EmailMessage # built-in
+from string import Template # using $, we can sbustitute variables
+from pathlib import Path # os.path; but better because we can read it
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Chav Sumalinog'
-email['to'] = 'chaviotics@gmail.com'
+email['to'] = 'chaviotics@gmail.com', 'epsumalinog@up.edu.ph'
 email['subject'] = 'First email from python.'
 
-email.set_content('Hello Chav!') # can send stuff
+email.set_content(html.substitute({'name': 'Chaviiiu'}), 'html') # can send stuff; # or even {'name': 'Isaac'}
 
 # smtp sends to email
 
@@ -17,4 +21,6 @@ with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.send_message(email)
     print('All good!')
 
+
+# learn how to send to multiple people at once with customized name using data bases.
 
